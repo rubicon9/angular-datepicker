8d79c17733c57ff79adf74b2d21ad84fb04c43d2
Vlad Ioffe <vlio20@gmail.com>
2017-04-22 22:06:05 +0300

    calendars-container progress 1

--- src/app/calendars-container/calendars-container.component.ts
+++ src/app/calendars-container/calendars-container.component.ts
@@ -1,14 +1,80 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
+import {Validator} from 'codelyzer/walkerFactory/walkerFn';
+import {ControlValueAccessor, FormControl, ValidationErrors} from '@angular/forms';
+import {CalendarValue, ECalendarValue, SingleCalendarValue} from '../common/types/calendar-value';
+import {Moment} from 'moment';
+import {UtilsService} from '../common/services/utils/utils.service';
+import {IDayCalendarConfig} from '../day-calendar/day-calendar-config.model';
+import {IMonthCalendarConfig} from '../month-calendar/month-calendar-config';
+import {CalendarType} from '../common/types/calendar-type';
 
 @Component({
   selector: 'dp-calendars-container',
   templateUrl: './calendars-container.component.html',
-  styleUrls: ['./calendars-container.component.less']
+  styleUrls: ['./calendars-container.component.less'],
+  providers: [UtilsService]
 })
-export class CalendarsContainerComponent implements OnInit {
+export class CalendarsContainerComponent implements OnInit,
+                                                    ControlValueAccessor,
+                                                    Validator {
+  @Input() config: IDayCalendarConfig | IMonthCalendarConfig;
+  @Input() type: CalendarType = 'day';
+  @Input() displayDate: SingleCalendarValue;
+  @Input() minDate: Moment | string;
+  @Input() maxDate: Moment | string;
 
-  constructor() { }
+  componentConfig: IDayCalendarConfig | IMonthCalendarConfig;
+  _selected: Moment[] = [];
+  inputValue: CalendarValue;
+  inputValueType: ECalendarValue;
+  validateFn: (FormControl, string) => {[key: string]: any};
+
+  constructor(public utilsService: UtilsService) {
+  }
 
   ngOnInit() {
+  }
+
+  init() {
+  }
+
+  set selected(selected: Moment[]) {
+    this._selected = selected;
+    this.onChangeCallback(this.processOnChangeCallback(selected));
+  }
+
+  get selected(): Moment[] {
+    return this._selected;
+  }
+
+  onChangeCallback(_: any) {
+  };
+
+  writeValue(value: CalendarValue): void {
+    this.inputValue = value;
+
+    if (value) {
+      this.selected = this.utilsService
+        .convertToMomentArray(value, this.componentConfig.format, this.componentConfig.allowMultiSelect);
+      this.init();
+    }
+  }
+
+  processOnChangeCallback(selected: Moment[]): CalendarValue {
+    return this.utilsService.convertFromMomentArray(this.componentConfig.format, selected, this.inputValueType);
+  }
+
+  registerOnChange(fn: any): void {
+  }
+
+  registerOnTouched(fn: any): void {
+  }
+
+  validate(formControl: FormControl): ValidationErrors | any {
+    if (this.minDate || this.maxDate) {
+      return this.validateFn(formControl, this.componentConfig.format);
+    } else {
+      return () => null;
+    }
   }
 }
